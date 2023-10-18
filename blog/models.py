13cd3243d132b41ea0c4fb84abba4a98adb34edd
ht93084 <antonio_tseng@pegatroncorp.com>
2023-10-18 15:43:27 +0800

    [UPDATE] Model Schema: Add image field

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,14 @@ from django.db import models
 class Articles(models.Model):
     title = models.CharField(max_length=50,)
     content = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/')
     create_at = models.DateTimeField(auto_now_add=True)
 
 def _get_all_articles():
     return Articles.objects.all()
+
+def _create_article(request):
+    Articles.objects.create(title=request.POST.get("title"), 
+                            content=request.POST.get("content"),
+                            image=request.FILES.get('image', 'None'))
+    return
