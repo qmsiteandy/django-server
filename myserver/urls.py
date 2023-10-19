115ac2ec7d8beda70a3f0f13f652e539f5ccb163
ht93084 <antonio_tseng@pegatroncorp.com>
2023-10-19 10:08:19 +0800

    [NEW] Blog Template: Retrieve Image

--- myserver/urls.py
+++ myserver/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from quickstart import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -27,4 +29,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('router/', include(router.urls)),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
